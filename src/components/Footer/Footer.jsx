11513d297b994e05b1af4189b2cb5d9c9fb4e091
radu <radustanciu841@yahoo.ro>
2021-09-03 13:49:01 +0300

    Add message save for donation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "../../components/Footer/Footer.css";
-
-function Footer() {
-  return (
-    <div className="footer">
-      <button type="button">Save for later</button>
-      <button type="button">Tell your friends</button>
-    </div>
-  );
+export class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDonationDisplay: false,
+    };
+  }
+  handleSaveDonation() {
+    this.setState({ isDonationDisplay: true });
+  }
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-button">
+          <button type="button" onClick={() => this.handleSaveDonation()}>
+            Save for later
+          </button>
+          <button type="button">Tell your friends</button>
+        </div>
+        <div className="donation-display">
+          {this.state.isDonationDisplay ? (
+            <em className="donation-save">Your donation is save</em>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
